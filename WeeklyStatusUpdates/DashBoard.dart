febb6deccdb80d12562355f99c7bf4fc59808b3d
saivamshikrishnagajji <137726056+saivamshikrishnagajji@users.noreply.github.com>
2024-04-08 20:51:41 -0500

    SaiVamshiKrishnaGajji-GDP-Team01-08April24

--- WeeklyStatusUpdates/DashBoard.dart
+++ WeeklyStatusUpdates/DashBoard.dart
@@ -10,3 +10,28 @@ import '../../helper/auth_service.dart';
 import '../../route.dart';
 import '../Widgets/Appbar.dart';
 import '../Widgets/chat_box.dart';
+
+
+
+
+
+   List<Users> sortedUsers = [];
+    for (int i = 0; i < users.length; i++) {
+      if (hasUnseenMessages[i]) {
+        Users user = users[i].copyWith(seen: true);
+        sortedUsers.insert(0, user);
+      } else {
+        sortedUsers.add(users[i]);
+      }
+    }
+
+    setState(() {
+      allUsers = sortedUsers;
+      csss = sortedUsers;
+      totaluser = users.length;
+      adminCount = users.where((user) => user.type == 'Admin').length;
+      foodDonorCount = users.where((user) => user.type == 'Food Donor').length;
+      recipientCount = users.where((user) => user.type == 'Recipient').length;
+      volunteerCount = users.where((user) => user.type == 'Volunteer').length;
+    });
+  }
