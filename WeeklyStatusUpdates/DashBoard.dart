82335fd8e4f4a0ae6094eddf5e6aa79eb52877cd
saivamshikrishnagajji <137726056+saivamshikrishnagajji@users.noreply.github.com>
2024-04-12 09:36:39 -0500

    SaiVamshiKrishnaGajji-GDP-Team01-12April24

--- WeeklyStatusUpdates/DashBoard.dart
+++ WeeklyStatusUpdates/DashBoard.dart
@@ -344,6 +344,31 @@ const SizedBox(
                                   ),
                                 ),
                               ),
+                              const SizedBox(
+                                width: 20,
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  filterUsers("Recipient");
+                                },
+                                child: Container(
+                                  width: 200,
+                                  decoration: BoxDecoration(
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: ((Colors.grey[300])!),
+                                          blurRadius: 4,
+                                          offset:
+                                              Offset(4, 8), // Shadow position
+                                        ),
+                                      ],
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(5)),
+                                  margin: EdgeInsets.all(10),
+                                  padding: EdgeInsets.all(30),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
 
 
 
